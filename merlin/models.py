1fc0e252132ae4bb17ac6b4e1247ce89ed3c666b
toosiki <pennygone@gmail.com>
2017-08-16 18:05:34 +0800

    view change

--- merlin/models.py
+++ merlin/models.py
@@ -6,6 +6,7 @@ class GifPics(models.Model):
     url = models.URLField(max_length=200)
     created_time = models.CharField(max_length=100)
     gif_index = models.IntegerField(primary_key=True)
+    likes = models.IntegerField(blank=True,default=0)
 
     def __str__(self):
         return self.title
--- source.py
+++ source.py
@@ -12,27 +12,41 @@ def get_gif(num):
         if soup.select('p > img') != [] :
             img_url = soup.select('p > img')[0].get('src')
             if 'gif' in img_url:
-                title = soup.select('p')[0].text.strip()
-                created_time = soup.select('div.info_text')[0].text[:10]
-                gif_index = num
-                # print(title,img_url,created_time,gif_index)
-                save_to_sqlite(title,img_url,created_time,gif_index)
-                print(str(num)+'saved')
+                res2 = requests.get(img_url)
+                if res2.history:
+                    pass
+                else:
+                    title = soup.select('p')[0].text.strip()
+                    created_time = soup.select('div.info_text')[0].text[:10]
+                    gif_index = num
+                    likes = int(soup.select('body > div:nth-of-type(2)')[0].text.split('\xa0')[0].strip('赞'))
+                    save_to_sqlite(title,img_url,created_time,gif_index,likes)
+                    print(str(num),'saved')
             else:
                 pass
         else:
             pass
-    except:
-        pass
+    except Exception as e:
+        print(e)
 
-def save_to_sqlite(title,url,created_time,gif_index):
+def save_to_sqlite(title,url,created_time,gif_index,likes):
     with lite.connect('db.sqlite3') as con:
         cur = con.cursor()
-        cur.execute("INSERT INTO merlin_gifpics (title,url,created_time,gif_index) Values (?,?,?,?)",(title,url,created_time,gif_index))
+        select = "SELECT * FROM merlin_gifpics WHERE gif_index={}".format(gif_index)
+        cur.execute(select)
+        res = cur.fetchall()
+        if len(res) >0:
+            update = "UPDATE merlin_gifpics set likes={} WHERE gif_index={}".format(likes,gif_index)
+            cur.execute(update)
+        else:
+            insert = "INSERT INTO merlin_gifpics (title, url, created_time, gif_index,likes) VALUES (?,?,?,?,?)"
+            cur.execute(insert,(title,url,created_time,gif_index,likes))
 
 if __name__ == '__main__':
     pool = Pool()
-    pool.map(get_gif,range(174901,200000))
+    pool.map(get_gif,range(1,180000))
+
+
 
 
 
