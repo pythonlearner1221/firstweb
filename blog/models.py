1c8c6b0432b6a63c7f5993cd856fe5827d171cb4
toosiki <pennygone@gmail.com>
2017-08-17 18:02:49 +0800

    change blog.view

--- blog/models.py
+++ blog/models.py
@@ -1,6 +1,9 @@
 from django.db import models
 from django.contrib.auth.models import User
 from django.urls import reverse
+import markdown
+from django.utils.html import strip_tags
+
 # Create your models here.
 
 class Category(models.Model):
@@ -24,6 +27,7 @@ class Post(models.Model):
     category = models.ForeignKey(Category)
     tags = models.ManyToManyField(Tag,blank=True)
     author = models.ForeignKey(User)
+    views = models.PositiveIntegerField(default=0)
 
     def __str__(self):
         return self.title
@@ -31,4 +35,15 @@ class Post(models.Model):
     def get_absolute_url(self):
         return reverse('blog:detail',kwargs={'pk':self.pk})
 
+    def increase_views(self):
+        self.views +=1
+        self.save(update_fields=['views'])
 
+    def save(self,*args,**kwargs):
+        if not self.excerpt:
+            md = markdown.Markdown(extensions=[
+                'markdown.extensions.extra',
+                'markdown.extensions.codehilite',
+            ])
+            self.excerpt = strip_tags(md.convert(self.body))[:54]
+        super(Post,self).save(*args,**kwargs)
